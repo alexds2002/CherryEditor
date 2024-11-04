5280f9bc586358df9b1fd8c68e2507f53bf68408
alexds2002 <alexds2002@gmail.com>
2024-11-04 11:31:33 +0200

    Restore terminal flags on exit to the original state
    Read input byte by byte

--- main.c
+++ main.c
@@ -1,12 +1,24 @@
+#include <stdlib.h>
 #include <termios.h>
 #include <unistd.h>
 
+// struct for the original state of the terminal, used to restore it back after exiting
+struct termios orig_termios;
+
+void disableRawMode() {
+  tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios);
+}
+
 void enableRawMode() {
-  struct termios raw;
-  tcgetattr(STDIN_FILENO, &raw);
-  raw.c_lflag &= ~(ECHO);
+  tcgetattr(STDIN_FILENO, &orig_termios);
+  // at exit functions to be called
+  atexit(disableRawMode);
+  struct termios raw = orig_termios;
+  // ICANON option starts reading byte by byte instead of line by line
+  raw.c_lflag &= ~(ECHO | ICANON);
   tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
 }
+
 int main() {
   enableRawMode();
   char c;
