63109b7286d07ec31b99e707ed9c857cd8c0841d
alexds2002 <alexds2002@gmail.com>
2024-11-05 08:24:26 +0200

    Seperate logic into funcitons

--- main.c
+++ main.c
@@ -6,6 +6,9 @@
 #include <termios.h>
 #include <unistd.h>
 
+/*** defines ***/
+#define CTRL_KEY(k) ((k) & 0x1f)
+
 /** Data **/
 // struct for the original state of the terminal, used to restore it back after exiting
 struct termios orig_termios;
@@ -38,16 +41,30 @@ void enableRawMode() {
   if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw) == -1) die("tcsetattr");
 }
 
+char editorReadKey() {
+  int nread;
+  char c;
+  while ((nread = read(STDIN_FILENO, &c, 1)) != 1) {
+    if (nread == -1 && errno != EAGAIN) die("read");
+  }
+  return c;
+}
+/*** input ***/
+void editorProcessKeypress() {
+  char c = editorReadKey();
+  switch (c) {
+    case CTRL_KEY('q'):
+      exit(0);
+      break;
+  }
+}
+
 int main() {
   enableRawMode();
+
   while (1) {
-    char c = '\0';
-    if (read(STDIN_FILENO, &c, 1) == -1 && errno != EAGAIN) die("read");
-    if (iscntrl(c)) {
-      printf("%d\r\n", c);
-    } else {
-      printf("%d ('%c')\r\n", c, c);
-    }
-    if (c == 'q') break;
-  }  return 0;
+    editorProcessKeypress();
+  }
+
+  return 0;
 }
